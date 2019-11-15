9c6fa19cea12622b0cb646720c331837515edd4b
Daryll Capistrano <daryllcapistrano@gmail.com>
2019-11-14 21:16:20 -0800

    add lightbox gallery

--- src/pages/photography/action.js
+++ src/pages/photography/action.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
 import SEO from '../../components/seo';
 import Layout from '../../components/layout';
+import PhotoGallery from '../../components/PhotoGallery';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,7 +13,9 @@ const RenameThis = () => (
 		<SEO title="RenameThis" />
 		<Container>
 			<Row>
-				<h1>INFO HERE</h1>
+				<div>
+					<PhotoGallery />
+				</div>
 			</Row>
 		</Container>
 	</Layout>

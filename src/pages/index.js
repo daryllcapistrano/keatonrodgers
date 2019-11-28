2ce1eeb29a0e0839132ae6ca37d2ccc0665992d1
Daryll Capistrano <daryllcapistrano@gmail.com>
2019-11-27 17:04:38 -0800

    add box shadow to video thumbnails

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,6 +29,11 @@ const IndexPage = () => (
 						<img
 							src="../../static/prana_manifest_farm-1d4e92cbc315a18832b2bb95dd261c67.png"
 							alt="Prana Manifest Farm"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
 						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
@@ -60,6 +65,11 @@ const IndexPage = () => (
 						<img
 							src="../../static/Screen Shot 2019-06-26 at 6.56.27 PM-fdaa1d8c0a415f9a741d131e08250201.png"
 							alt="Bittersland"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
 						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
@@ -88,7 +98,15 @@ const IndexPage = () => (
 					}}
 				>
 					<Link to="/video/cruisingintherain/">
-						<img src="../../static/rainthumb-28e162c7d7c85ed3a272a9bf18a0e4b3.png" alt="Cruising in the Rain" />
+						<img
+							src="../../static/rainthumb-28e162c7d7c85ed3a272a9bf18a0e4b3.png"
+							alt="Cruising in the Rain"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
+						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
 								style={{
@@ -116,7 +134,15 @@ const IndexPage = () => (
 					}}
 				>
 					<Link to="/video/chromeindustries/">
-						<img src="../../static/chromeindustries-58ed57e84737453f1eac16261cee2f7c.png" alt="Chrome Industries" />
+						<img
+							src="../../static/chromeindustries-58ed57e84737453f1eac16261cee2f7c.png"
+							alt="Chrome Industries"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
+						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
 								style={{
@@ -144,7 +170,15 @@ const IndexPage = () => (
 					}}
 				>
 					<Link to="/video/greendream/">
-						<img src="../../static/greendream-7f09b0b5919904a4de37f0f0dff4102f.png" alt="Green Dream" />
+						<img
+							src="../../static/greendream-7f09b0b5919904a4de37f0f0dff4102f.png"
+							alt="Green Dream"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
+						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
 								style={{
@@ -172,7 +206,15 @@ const IndexPage = () => (
 					}}
 				>
 					<Link to="/video/jackgraydon/">
-						<img src="../../static/jackgraydon-2450058223da44fceccf67ca153079de.png" alt="Jack Graydon" />
+						<img
+							src="../../static/jackgraydon-2450058223da44fceccf67ca153079de.png"
+							alt="Jack Graydon"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
+						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
 								style={{
@@ -200,7 +242,15 @@ const IndexPage = () => (
 					}}
 				>
 					<Link to="/video/ccs/">
-						<img src="../../static/CCS-ef15a145827871a474971cb22372bb5c.png" alt="CCS" />
+						<img
+							src="../../static/CCS-ef15a145827871a474971cb22372bb5c.png"
+							alt="CCS"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
+						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
 								style={{
@@ -228,7 +278,15 @@ const IndexPage = () => (
 					}}
 				>
 					<Link to="/video/alpental/">
-						<img src="../../static/Alpental-33b4fd5b6e239eb7c7c98729b3760236.png" alt="Alpental" />
+						<img
+							src="../../static/Alpental-33b4fd5b6e239eb7c7c98729b3760236.png"
+							alt="Alpental"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
+						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
 								style={{
@@ -256,7 +314,15 @@ const IndexPage = () => (
 					}}
 				>
 					<Link to="/video/thrasher/">
-						<img src="../../static/thrasher-83c9ee1e7383803a9ed04b6c9dfeda94.png" alt="Thrasher" />
+						<img
+							src="../../static/thrasher-83c9ee1e7383803a9ed04b6c9dfeda94.png"
+							alt="Thrasher"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
+						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title
 								style={{
@@ -287,6 +353,11 @@ const IndexPage = () => (
 						<img
 							src="../../static/The Summit at Snoqualmie- sprung-e539868e30d606b3e3ac3ac29a6c9774.png"
 							alt="Summit at Snoqualmie"
+							style={{
+								webkitBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								mozBoxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6)',
+								boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.6),'
+							}}
 						/>
 						<Card.ImgOverlay className="text-center">
 							<Card.Title

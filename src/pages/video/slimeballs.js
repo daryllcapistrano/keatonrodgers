ae99aa67ded861308e9e66ef003bd50ac59109c3
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-01-30 19:35:33 -0800

    add new videos to slimeballs.js

--- src/pages/video/slimeballs.js
+++ src/pages/video/slimeballs.js
@@ -34,10 +34,114 @@ const Slimeballs = () => (
 		<Container>
 			<Row class="embed-responsive embed-responsive-16by9">
 				<iframe
-					title="Slimeballs"
+					title="SlimeballsPromo1"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/mo8VYHI6aes"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo2"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/q25eR8f1yro"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo3"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/m13QqtAeHxc"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo4"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/CjxfdhOvT4k"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo5"
 					width="100%"
 					height="480"
 					src="https://www.youtube.com/embed/69BABGRWz0o"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo6"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/BYYeqrtZqCg"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo7"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/yK9CS95xQjA"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo8"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/R23jFPaaNLE"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen="true"
+				>
+					...
+				</iframe>
+			</Row>
+			<Row class="embed-responsive embed-responsive-16by9">
+				<iframe
+					title="SlimeballsPromo9"
+					width="100%"
+					height="480"
+					src="https://www.youtube.com/embed/yjHIyzIhBUM"
 					frameborder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
 					allowfullscreen="true"
